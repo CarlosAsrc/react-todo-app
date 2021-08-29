39c5f03f760ae6adfe7cadca18323df1eadaac27
ilegra.rodrigues <ilegra.rodrigues>
2021-08-29 05:10:26 -0300

    Add 404.html for route redirect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css'
 
@@ -8,7 +8,7 @@ import TodoContainer from './components/TodoContainer'
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <TodoContainer />
         </Router>
     </React.StrictMode>,
